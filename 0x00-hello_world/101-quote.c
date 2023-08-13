d2c8af19b4ea5e5ba38c789fe555d352cef0487c
“johnsmartaidoo” <johnsmartaidoo9@gmail.com>
2023-08-13 07:02:59 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,13 @@
 #include <fcntl.h>
 #include <string.h>
 /**
- * main -  A C program that works
+ * main - A C program that prints a new line the standard
  * Return: 1 (successful)
  */
 
 int main(void)
 {
-    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int fd = STDERR_FILENO;
-    write(fd, message, strlen(message));
-    return 1;
+	const char * "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	return (1);
 }
