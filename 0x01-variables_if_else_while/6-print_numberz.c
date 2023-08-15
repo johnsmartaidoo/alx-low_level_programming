502c2a2c843760ff03df941aff1a218c365eddb6
“johnsmartaidoo” <johnsmartaidoo9@gmail.com>
2023-08-14 22:21:15 -0700

    print numbers 2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/** main - A c program that prints numbers using putchar
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+
+	return 0;
+}
