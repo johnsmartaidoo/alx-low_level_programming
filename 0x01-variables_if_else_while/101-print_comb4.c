de1fbcb773da43b78ea328b6b81919b33ece109b
“johnsmartaidoo” <johnsmartaidoo9@gmail.com>
2023-08-14 22:51:30 -0700

    print comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/** main - A c program that uses for to print comb.
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int i, j, k;
+	
+	for (i = 0; i < 10; i++) {
+	for (j = i + 1; j < 10; j++) {
+	for (k = j + 1; k < 10; k++) {
+	if (i != j && i != k && j != k) {
+	putchar(i + '0');
+	putchar(j + '0');
+	putchar(k + '0');
+	if (i != 7 || j != 8 || k != 9) {
+	putchar(',');
+	putchar(' ')
+	}
+	}
+	}
+	}
+	}
+	
+	putchar('\n');
+	
+	return 0;
+}
