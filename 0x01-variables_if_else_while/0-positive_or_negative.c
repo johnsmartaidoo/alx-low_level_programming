da9c15c4eb58d4df3e44f891aa015a5a128d7917
“johnsmartaidoo” <johnsmartaidoo9@gmail.com>
2023-08-14 21:44:55 -0700

    positive and negatice

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,21 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/** main - Entry point
- * Return: Always 0 (successful)
+/** main - Enter the exact string and value
+ * Return: 0 (successful)
  */
 
 int main(void)
 {
 	int n;
 
-	srand(time(0));
+	srand(time(0)	
 	n = rand() - RAND_MAX / 2;
+	printf("%d ", n);
+
 	if (n > 0)
-		printf("%d is positive\n", n);
+	printf("is positive\n");
+
 	else if (n == 0)
-		printf("%d is zero\n", n);
-	else 
-		printf("%d is negetive\n", n)
+	printf("is zero\n");
+
+	 else
+	printf("is negative\n");
+
 	return (0);
 }
