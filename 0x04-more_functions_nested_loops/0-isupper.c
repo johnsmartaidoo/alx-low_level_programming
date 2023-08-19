ace6a91968e94c394849dee6daa544da162dc760
“johnsmartaidoo” <johnsmartaidoo9@gmail.com>
2023-08-18 21:19:52 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio>
+/** main - fuctions master
+ * Return: Always 0
+ */
+
+ int.main(void)
+ int _isupper(int c) {
+    if (c >= 'A' && c <= 'Z') {
+        return 1;  // uppercase character
+    } else {
+        return 0;  // not uppercase character
+    }
+}
