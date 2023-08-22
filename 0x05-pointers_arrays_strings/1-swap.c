f2eec4096eda741d4708a2f6aa2e757d3f0005f4
“johnsmartaidoo” <johnsmartaidoo9@gmail.com>
2023-08-22 09:05:13 -0700

    puys

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,5 @@
 void swap_int(int *a, int *b) {
-    int temp = *a; // save the value of a in a temporary variable
-    *a = *b; // assign the value of b to a
-    *b = temp; // assign the value of the temporary variable to b
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
