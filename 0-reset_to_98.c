e1dd67f9ef884ba2187ae5824f538accd8a8e8ed
“johnsmartaidoo” <johnsmartaidoo9@gmail.com>
2023-08-22 08:50:23 -0700

    array

---
+++ 0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/** main - Arrays print
+ * Return: 0
+ */
+
+int main(void)
+	void reset_to_98(int *n) {
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include <stdio>
+/** main - print arrays
+ * return: 0
+ */
+
+void reset_to_98(int *n) {
+    *n = 98;
+}
